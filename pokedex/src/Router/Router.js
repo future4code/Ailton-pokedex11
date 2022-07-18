d04eac18abaaaf619a4c5f07b2edb564b0241989
Patrick de Melo Ferraz <patrick.ferraz66@gmail.com>
2022-07-18 16:25:42 -0300

    pokedex1

---
+++ pokedex/src/Router/Router.js
@@ -0,0 +1,17 @@
+import { BrowserRouter,Routes,Route } from "react-router-dom";
+import React from 'react'
+import Home from "../Pages/Home.js";
+import Pokedex from "../Pages/Pokedex";
+import Details from "../Pages/Details";
+
+export default function Router() {
+  return (
+    <BrowserRouter>
+    <Routes>
+        <Route index element={<Home/>}/>
+        <Route path ="pokedex" element={<Pokedex/>}/>
+        <Route path ="details" element={<Details/>}/>
+    </Routes>
+    </BrowserRouter>
+  )
+}
