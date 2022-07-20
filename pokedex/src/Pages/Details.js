e12126929af12f1d22cc1735868fa71558347584
Patrick de Melo Ferraz <patrick.ferraz66@gmail.com>
2022-07-20 12:04:23 -0300

    Pokemons com 2 tipos sendo renderizados, Background mundado de cor de acordo com os tipos de pokemons, pág de detalhes pronta para estruturar

--- pokedex/src/Pages/Details.js
+++ pokedex/src/Pages/Details.js
@@ -1,12 +1,36 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import { goToHomePage } from '../Router/coordinator'
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
+import { useState,useEffect } from 'react';
 
 export default function Details() {
+  const params = useParams();
+  const [details, setDetails] = useState([]);
   const navigate = useNavigate()
+
+  console.log(params)
+
+  useEffect(() => {
+    axios
+      .get(`https://pokeapi.co/api/v2/pokemon/${params.id}`)
+      .then((res) => {
+        console.log('Os detalhes:', res.data)
+      setDetails(res.data)
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+
+
+
   return (
     <div>
       <h1>Detalhes</h1>
+    <p>{details.name}</p>
       <button onClick={()=>goToHomePage(navigate)}>Home</button>
       </div>
   )
