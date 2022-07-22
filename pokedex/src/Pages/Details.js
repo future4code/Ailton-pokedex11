4c67ca3dc904511602db86cac84410624259cc85
Patrick de Melo Ferraz <patrick.ferraz66@gmail.com>
2022-07-22 10:15:31 -0300

    Progress bar adicionada nos stats

--- pokedex/src/Pages/Details.js
+++ pokedex/src/Pages/Details.js
@@ -6,6 +6,7 @@ import { useParams } from 'react-router-dom';
 import { useState,useEffect } from 'react';
 import styled from 'styled-components';
 import Header from '../components/Header';
+import LinearProgress from "@mui/material/LinearProgress"
 
 
 const CardDetalhes = styled.div`
@@ -104,11 +105,17 @@ export default function Details() {
       <DivBaseStats>
       <h4>Base States</h4>
     <p>{pokemon.stats === undefined ? "" : pokemon.stats[0].stat.name}: {pokemon.stats === undefined ? <p>carregando</p> : pokemon.stats[0].base_stat}</p>
+    <LinearProgress variant="determinate"  value={pokemon.stats !== undefined ?pokemon.stats[0].base_stat:"" } />
     <p>{pokemon.stats === undefined ? "" : pokemon.stats[1].stat.name}: {pokemon.stats === undefined ? <p>carregando</p> : pokemon.stats[1].base_stat}</p>
+    <LinearProgress variant="determinate"  value={pokemon.stats !== undefined ?pokemon.stats[1].base_stat:"" } />
     <p>{pokemon.stats === undefined ? "" : pokemon.stats[2].stat.name}: {pokemon.stats === undefined ? <p>carregando</p> : pokemon.stats[2].base_stat}</p>
+    <LinearProgress variant="determinate"  value={pokemon.stats !== undefined ?pokemon.stats[2].base_stat:"" } />
     <p>{pokemon.stats === undefined ? "" : pokemon.stats[3].stat.name}: {pokemon.stats === undefined ? <p>carregando</p> : pokemon.stats[3].base_stat}</p>
+    <LinearProgress variant="determinate"  value={pokemon.stats !== undefined ?pokemon.stats[3].base_stat:"" } />
     <p>{pokemon.stats === undefined ? "" : pokemon.stats[4].stat.name}: {pokemon.stats === undefined ? <p>carregando</p> : pokemon.stats[4].base_stat}</p>
+    <LinearProgress variant="determinate"  value={pokemon.stats !== undefined ?pokemon.stats[4].base_stat:"" } />
     <p>{pokemon.stats === undefined ? "" : pokemon.stats[5].stat.name}: {pokemon.stats === undefined ? <p>carregando</p> : pokemon.stats[5].base_stat}</p>
+    <LinearProgress variant="determinate"  value={pokemon.stats !== undefined ?pokemon.stats[5].base_stat:"" } />
     </DivBaseStats>
 
     <div>
