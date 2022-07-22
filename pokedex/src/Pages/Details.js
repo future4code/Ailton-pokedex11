a275fe5d8017efe68d4a86c6bdcdb599963c75b4
Patrick de Melo Ferraz <patrick.ferraz66@gmail.com>
2022-07-22 07:45:12 -0300

    Arrumei uma besteirinha

--- pokedex/src/Pages/Details.js
+++ pokedex/src/Pages/Details.js
@@ -50,30 +50,31 @@ export default function Details() {
       <h1>Detalhes</h1>
       <CardDetalhes>
       <DivImgs>
-      <img src={pokemon.sprites === undefined ? <p>carregando</p>: pokemon.sprites.front_default}/>
-      <img src={pokemon.sprites === undefined ? <p>carregando</p>: pokemon.sprites.back_default}/>
+      <img src={pokemon.sprites === undefined ? "": pokemon.sprites.front_default}/>
+      <img src={pokemon.sprites === undefined ? "": pokemon.sprites.back_default}/>
 
       </DivImgs>
       <DivBaseStats>
       <h4>Base States</h4>
-    <p>{pokemon.stats === undefined ? <p>carregando</p> : pokemon.stats[0].stat.name}: {pokemon.stats === undefined ? <p>carregando</p> : pokemon.stats[0].base_stat}</p>
-    <p>{pokemon.stats === undefined ? <p>carregando</p> : pokemon.stats[1].stat.name}: {pokemon.stats === undefined ? <p>carregando</p> : pokemon.stats[1].base_stat}</p>
-    <p>{pokemon.stats === undefined ? <p>carregando</p> : pokemon.stats[2].stat.name}: {pokemon.stats === undefined ? <p>carregando</p> : pokemon.stats[2].base_stat}</p>
-    <p>{pokemon.stats === undefined ? <p>carregando</p> : pokemon.stats[3].stat.name}: {pokemon.stats === undefined ? <p>carregando</p> : pokemon.stats[3].base_stat}</p>
-    <p>{pokemon.stats === undefined ? <p>carregando</p> : pokemon.stats[4].stat.name}: {pokemon.stats === undefined ? <p>carregando</p> : pokemon.stats[4].base_stat}</p>
-    <p>{pokemon.stats === undefined ? <p>carregando</p> : pokemon.stats[5].stat.name}: {pokemon.stats === undefined ? <p>carregando</p> : pokemon.stats[5].base_stat}</p>
+    <p>{pokemon.stats === undefined ? "" : pokemon.stats[0].stat.name}: {pokemon.stats === undefined ? <p>carregando</p> : pokemon.stats[0].base_stat}</p>
+    <p>{pokemon.stats === undefined ? "" : pokemon.stats[1].stat.name}: {pokemon.stats === undefined ? <p>carregando</p> : pokemon.stats[1].base_stat}</p>
+    <p>{pokemon.stats === undefined ? "" : pokemon.stats[2].stat.name}: {pokemon.stats === undefined ? <p>carregando</p> : pokemon.stats[2].base_stat}</p>
+    <p>{pokemon.stats === undefined ? "" : pokemon.stats[3].stat.name}: {pokemon.stats === undefined ? <p>carregando</p> : pokemon.stats[3].base_stat}</p>
+    <p>{pokemon.stats === undefined ? "" : pokemon.stats[4].stat.name}: {pokemon.stats === undefined ? <p>carregando</p> : pokemon.stats[4].base_stat}</p>
+    <p>{pokemon.stats === undefined ? "" : pokemon.stats[5].stat.name}: {pokemon.stats === undefined ? <p>carregando</p> : pokemon.stats[5].base_stat}</p>
     </DivBaseStats>
 
     <div>
     <p>#{pokemon.id}</p>
-    <p>
-    {pokemon.states === undefined ? <p>carregando</p> : pokemon.name[0].toUpperCase()}
-    </p>
-    <p>{pokemon.types === undefined ? <p>carregando</p> : pokemon.types[0]?.type.name}</p>
-    <p>{pokemon.types === undefined ? <p>carregando</p> :pokemon.types[1]?.type.name}</p>
+    <h4>
+    {pokemon.name === undefined ?"" : pokemon.name[0].toUpperCase()}
+    {pokemon.name === undefined ?"" :pokemon.name.slice(1)}
+    </h4>
+    <p>{pokemon.types === undefined ? "" : pokemon.types[0]?.type.name}</p>
+    <p>{pokemon.types === undefined ? "" :pokemon.types[1]?.type.name}</p>
     <img
           alt={pokemon.name}
-          src={pokemon.types === undefined? <p>carregando</p> : pokemon.sprites.other["official-artwork"].front_default}
+          src={pokemon.sprites === undefined? <p>carregando</p> : pokemon.sprites.other["official-artwork"].front_default}
           height={110}
           width={110}
         />
@@ -82,10 +83,10 @@ export default function Details() {
 
     <div>
     <h4>Moves</h4>
-    <p>{pokemon.moves === undefined ? <p>carregando</p>: pokemon.moves[0].move.name}</p>
-    <p>{pokemon.moves === undefined ? <p>carregando</p>:pokemon.moves[1].move.name}</p>
-    <p>{pokemon.moves === undefined ? <p>carregando</p>:pokemon.moves[2].move.name}</p>
-    <p>{pokemon.moves === undefined ? <p>carregando</p>:pokemon.moves[4].move.name}</p>
+    <p>{pokemon.moves === undefined ? "": pokemon.moves[0].move.name}</p>
+    <p>{pokemon.moves === undefined ? "":pokemon.moves[1].move.name}</p>
+    <p>{pokemon.moves === undefined ? "":pokemon.moves[2].move.name}</p>
+    <p>{pokemon.moves === undefined ? "":pokemon.moves[4].move.name}</p>
     </div>
     
     </CardDetalhes>
@@ -93,3 +94,4 @@ export default function Details() {
       </div>
   )
 }
+  
