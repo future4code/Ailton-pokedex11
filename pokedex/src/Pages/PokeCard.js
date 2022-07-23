7ddf4ba02973a36143e5e574f13a7993498c1b56
Patrick de Melo Ferraz <patrick.ferraz66@gmail.com>
2022-07-23 10:42:30 -0300

    Icones dos tipos adicionado e funcionando

--- pokedex/src/Pages/PokeCard.js
+++ pokedex/src/Pages/PokeCard.js
@@ -2,6 +2,27 @@ import React, { useState } from 'react'
 import axios from 'axios'
 import styled from 'styled-components'
 import { useNavigate, useParams } from 'react-router-dom'
+import bug from '../imgs/Bug.png'
+import dark from '../imgs/Dark.png'
+import dragon from '../imgs/Dragon.png'
+import electric from '../imgs/Electric.png'
+import fairy from '../imgs/Fairy.png'
+import fighting from '../imgs/Fighting.png'
+import fire from '../imgs/Fire.png'
+import flying from '../imgs/Flying.png'
+import grass from '../imgs/Grass.png'
+import ground from '../imgs/Ground.png'
+import normal from '../imgs/Normal.png'
+import poison from '../imgs/Poison.png'
+import psychic from '../imgs/Psychic.png'
+import rock from '../imgs/Rock.png'
+import steel from '../imgs/Steel.png'
+import water from '../imgs/Water.png'
+import ghost from '../imgs/Ghost.png'
+import ice from '../imgs/Ice.png'
+
+
+
 const Card = styled.div`
   border: 1px solid black;
   height: 36vh;
@@ -138,9 +159,62 @@ const corBackGround = (type) => {
       return 'black'
   }
 }
+
+
+
 export default function PokeCard({ poke, setPokedex }) {
   const navigate = useNavigate()
   const [click, setClick] = useState(false)
+
+  const pokeType =
+
+    poke.types.map((tipo)=>{
+      switch (tipo.type.name) {
+        case 'normal':
+          return <img src={normal}/>
+        case 'fighting':
+          return <img src={fighting}/>
+        case 'flying':
+          return <img src={flying}/>
+        case 'poison':
+          return <img src={poison}/>
+        case 'ground':
+          return <img src={ground}/>
+        case 'rock':
+          return <img src={rock}/>
+        case 'bug':
+          return <img src={bug}/>
+        case 'ghost':
+          return <img src={ghost}/>
+        case 'steel':
+          return <img src={steel}/>
+        case 'fire':
+          return <img src={fire}/>
+        case 'water':
+          return <img src={water}/>
+        case 'grass':
+          return <img src={grass}/>
+        case 'electric':
+          return <img src={electric}/>
+        case 'psychic':
+          return <img src={psychic}/>
+        case 'ice':
+          return <img src={ice}/>
+        case 'dragon':
+          return <img src={dragon}/>
+        case 'dark':
+          return <img src={dark}/>
+        case 'fairy':
+          return <img src={fairy}/>
+        default:
+          return 'black'
+      }
+
+    })
+  
+ 
+
+
   return (
     <Card
       key={poke.id}
@@ -168,8 +242,7 @@ export default function PokeCard({ poke, setPokedex }) {
       </DivImagem>
       <DivInfo>
         <DivTipos>
-          <p>{poke.types[0].type.name}</p>
-          <p>{poke.types[1]?.type.name}</p>
+          <p>{pokeType}</p>
         </DivTipos>
 
       </DivInfo>
